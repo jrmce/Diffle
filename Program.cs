ad7447718401a069fe04eadd7608c5b6e5e2597c
JR McEntee <6009809+jrmce@users.noreply.github.com>
2022-11-13 17:25:49 -0500

    remove

--- Program.cs
+++ Program.cs
@@ -9,5 +9,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
 await builder.Build().RunAsync();
